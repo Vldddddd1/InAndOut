7cd1f7622a10d55e749c4872452279a4419527f7
robert <robertzglobiu29@gmail.com>
2025-12-14 14:33:28 +0200

    added images for the home carousel
    we need a way to add a little bigger images in the same space because they look really low quality. but i also need to create a few personal assets of food to add there.

--- src/components/Landing/Carousel/images.ts
+++ src/components/Landing/Carousel/images.ts
@@ -2,14 +2,14 @@
 //200x200 sa arate bn
 
 const EImages = {
-    image1: "https://placehold.co/225",
-    image2: "https://placehold.co/300",
-    image3: "https://placehold.co/325",
-    image4: "https://placehold.co/400",
-    image5: "https://placehold.co/425",
-    image6: "https://placehold.co/500",
-    image7: "https://placehold.co/500",
-    image8: "https://placehold.co/500",
+    image1: "./src/assets/images/carouselImages/frigaruie.png",
+    image2: "./src/assets/images/carouselImages/tomato.jpg",
+    image3: "./src/assets/images/carouselImages/grapefruit.jpg",
+    image4: "./src/assets/images/carouselImages/kiwi.jpg",
+    image5: "./src/assets/images/carouselImages/peste.jpg",
+    image6: "./src/assets/images/carouselImages/salata.jpg",
+    image7: "./src/assets/images/carouselImages/pasta.jpg",
+    image8: "./src/assets/images/carouselImages/paine.jpg",
 }
 
 export default EImages;
